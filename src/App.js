c6699a39f726ead7edc2d6b4a3d1d583d26a7adf
serenomoon <saulofernandezibar@gmail.com>
2022-07-26 15:59:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import logo from './logo.svg';
 import './App.css';
+import { decrement, increment, incrementBy } from './store/slices/counter';
 
 function App() {
+
+  const { counter } = useSelector( state => state.counter );
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>count is: { counter }</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button type='button' onClick={ () => dispatch( increment() ) } >Increment</button>
+
+          <button type='button' onClick={ () => dispatch( decrement() ) } >Decrement</button>
+          
+          <button type='button' onClick={ () => dispatch( incrementBy(2) ) } >IncrementBy</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/PokemonApp.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPokemons } from "./store/slices/pokemon/thunks";
+
+export const PokemonApp = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch( getPokemons() )
+    
+    }, [])
+    
+
+  return (
+    <>
+    <h1>PokemonApp</h1>
+
+    <hr/>
+
+    <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+        <li>Hola</li>
+    </ul>
+    </>
+  )
+}
